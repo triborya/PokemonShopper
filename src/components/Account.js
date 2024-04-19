371c05971515485937b084314bc49666b0ced45f
triborrya <gorobcovdmitrie@gmail.com>
2024-04-19 17:34:22 +0200

    siminal

--- src/components/Account.js
+++ src/components/Account.js
@@ -42,70 +42,70 @@ const Account = ({ token, user, setUser }) => {
   };
 
   return (
-    <div className='ui text container'>
-      <div className='ui segment'>
-        <h1 className='ui dividing header'>Account Info</h1>
-        <form className='ui form' onSubmit={handleSubmit}>
-          <div className='two fields'>
-            <div className='field'>
-              <label htmlFor='firstName'>First Name</label>
+    <div className="ui text container">
+      <div className="ui segment">
+        <h1 className="ui dividing header">Account Info</h1>
+        <form className="ui form" onSubmit={handleSubmit}>
+          <div className="two fields">
+            <div className="field">
+              <label htmlFor="firstName">First Name</label>
               <input
-                type='text'
-                name='firstName'
-                id='firstName'
+                type="text"
+                name="firstName"
+                id="firstName"
                 value={firstName}
                 onChange={(e) => setFirstName(e.target.value)}
               />
             </div>
-            <div className='field'>
-              <label htmlFor='lastName'>Last Name</label>
+            <div className="field">
+              <label htmlFor="lastName">Last Name</label>
               <input
-                type='text'
-                name='lastName'
-                id='lastName'
+                type="text"
+                name="lastName"
+                id="lastName"
                 value={lastName}
                 onChange={(e) => setLastName(e.target.value)}
               />
             </div>
           </div>
-          <div className='field'>
-            <label htmlFor='username'>Username</label>
+          <div className="field">
+            <label htmlFor="username">Username</label>
             <input
-              type='text'
-              name='username'
-              id='username'
+              type="text"
+              name="username"
+              id="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
           </div>
-          <div className='required field'>
-            <label htmlFor='emailAddress'>Email Address</label>
+          <div className="required field">
+            <label htmlFor="emailAddress">Email Address</label>
             <input
-              type='text'
-              name='emailAddress'
-              id='emailAddress'
+              type="text"
+              name="emailAddress"
+              id="emailAddress"
               value={emailAddress}
               onChange={(e) => setEmailAddress(e.target.value)}
             />
           </div>
-          <div className='field'>
-            <label htmlFor='phoneNumber'>Phone Number</label>
+          <div className="field">
+            <label htmlFor="phoneNumber">Phone Number</label>
             <input
-              type='text'
-              name='phoneNumber'
-              id='phoneNumber'
+              type="text"
+              name="phoneNumber"
+              id="phoneNumber"
               value={phoneNumber || ""}
               onChange={(e) => setPhoneNumber(e.target.value)}
             />
           </div>
-          <button className='ui primary submit button' type='submit'>
+          <button className="ui primary submit button" type="submit">
             Update
           </button>
           {error && (
-            <div className='ui bottom attached error message'>{error}</div>
+            <div className="ui bottom attached error message">{error}</div>
           )}
           {success && (
-            <div className='ui center aligned inverted green segment'>
+            <div className="ui center aligned inverted green segment">
               "Account updated successfully!"
             </div>
           )}
